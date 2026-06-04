eb9caef2a2cb96b817566fec369c6ed90f9b9631
Ricardo Rhillen <rhillen5@gmail.com>
2026-06-04 12:58:54 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyC2iQbYA15uEHA2v9w0rX5DsVw8iHnL9JU", 
-  authDomain: "nextn-f5a13.firebaseapp.com",
-  projectId: "nextn-f5a13",
-  storageBucket: "nextn-f5a13.firebasestorage.app",
+  authDomain: "studio-7939397762-14c24.firebaseapp.com",
+  projectId: "studio-7939397762-14c24",
+  storageBucket: "studio-7939397762-14c24.firebasestorage.app",
   messagingSenderId: "990646274128",
   appId: "1:990646274128:web:65f7c35f2d65c3b5",
 };
